982b477fe49c369d7964efacbddec8fa360275d9
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-06-26 00:47:35 +0900

    create : POST 장바구니 굿즈 추가 api 구현

--- routers/goods.js
+++ routers/goods.js
@@ -1,7 +1,26 @@
 const express = require("express");
+
 const Goods = require("../schemas/goods");
+const Cart = require("../schemas/cart");
 
 const router = express.Router();
+
+router.post("/:goodsId/cart", async (req, res) => {
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  const existsCarts = await Cart.find({ goodsId });
+  if (existsCarts.length) {
+    return res.status(400).json({
+      success: false,
+      errorMessage: "이미 장바구니에 해당하는 상품이 존재합니다.",
+    });
+  }
+
+  await Cart.create({ goodsId, quantity });
+
+  res.json({ result: "success" });
+});
 
 router.post("/", async (req, res) => {
   try {
