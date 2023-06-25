6dd726f0928515aa87448d5b47c6404f7db89c05
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-06-25 19:35:04 +0900

    create : mongoDB 연결

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connect = require("./schemas/index");
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+connect();
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
---
+++ schemas/index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO_URL, {
+      dbName: "buckets",
+    })
+    .then(() => console.log("몽고 연결 완료"))
+    .catch((err) => console.error(err));
+};
+
+mongoose.connection.on("error", (err) => {
+  console.error("몽고 연결 에러", err);
+});
+
+module.exports = connect;
